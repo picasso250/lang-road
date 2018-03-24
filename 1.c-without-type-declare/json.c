2d20c03ba4544f73c1ade241d1b20c0e47fc858a
王霄池(wangxiaochi) <cumt.xiaochi@gmail.com>
2018-03-24 10:36:38 +0800

    Update json.c

--- 1.c-without-type-declare/json.c
+++ 1.c-without-type-declare/json.c
@@ -1,1 +1,44 @@
+typedef struct _json_object {
+	int size;
+	json_key_value *kv;
+} json_object;
+typedef struct _json_key_value {
+	struct _json_string key;
+	struct _json value;
+} json_key_value;
+typedef struct _json_array {
+	int size;
+	struct _json;
+} json_array;
+typedef struct _json_string {
+	int size;
+	char *str;
+} json_string;
+typedef struct _json_number {
+	int type;
+	union {
+		long i;
+		double f;
+	};
+} json_number;
 
+typedef struct _json {
+	int type;
+	union {
+		json_object o;
+		json_array a;
+		json_string s;
+		json_number n;
+	};
+} json;
+enum JsonType {
+	object = 1;
+	array = 2;
+	string = 3;
+	number = 4;
+	true_ = 5;
+	false_ = 6;
+	null_ = 7;
+	int_ = 8;
+	float_ = 9;
+}; 
