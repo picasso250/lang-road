ea5fba332a5da570afdbe9084aa49ff24467b055
xiaochi <cumt.xiaochi@gmail.com>
2018-05-12 10:57:03 +0800

    change test

--- 1.c-like/test_lex.php
+++ 1.c-like/test_lex.php
@@ -1,9 +1,12 @@
 <?php
 
-function test_lex_line() {
-    assert_true(lex_line("a+=foo")==[['word','a'],['operator','+='],['word','foo']]);
+function test_lex() {
+    assert_true(lex("a+=foo")== [new LexNode('word','a',1),new LexNode('operator','+=',1),new LexNode('word','foo',1)]);
     //print_r(lex_line("a+=foo"));
-    assert_true(lex_line('"abc"//bc')==[['string','abc'],['comment','bc'],]);
-    assert_true(lex_line('0.2f')==[['number','0.2f'],]);
+    assert_true(lex('"abc"//bc')==[new LexNode('string','abc',1),new LexNode('comment','bc',1),]);
+    assert_true(lex('0.2f')==[new LexNode('number','0.2f',1),]);
     //print_r(lex_line('0.2f'));
+    assert_true(lex("func main(){\n0\n}")
+        ==[new LexNode("word", "func", 1),new LexNode("word","main",1),new LexNode("operator","(",1), new LexNode("operator", ")",1), new LexNode("operator","{",1),new LexNode("number","0",2),new LexNode("operator","}",3)]);
+    //print_r(lex("func main(){\n0\n}"));
 }
